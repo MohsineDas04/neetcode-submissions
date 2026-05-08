ede5c37b8cc854fa370a3ace0346f48f98178f0c
Mohsine Lakhlil <125510008+MohsineDas04@users.noreply.github.com>
2026-05-08 03:04:25 +0100

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,23 @@
+from random import randint, Random
+
+
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        self.key = (
+            randint(0, 9999999)
+            + randint(0, 9999999)
+            + randint(0, 9999999)
+            - randint(0, 659) / randint(10, 555)
+        )
+        self.key = str(self.key)
+        self.encoded = ""
+        self.ll = strs
+        for s in strs:
+            self.encoded = self.encoded + f"{s}{self.key}"
+        # print(f"encoded : {self.encoded}")
+        return self.encoded
+
+    def decode(self, s: str) -> List[str]:
+        self.decoded = self.encoded.split(self.key)
+        self.decoded.pop(-1)
+        return self.decoded
